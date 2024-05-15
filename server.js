fb0dd5be0a3767db21b2838e6f755913dd7ef7c9
Kelden P Drac <02230285.cst@rub.edu.bt>
2024-05-16 01:13:15 +0600

    port2

--- server.js
+++ server.js
@@ -408,5 +408,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${port}`);
 });
